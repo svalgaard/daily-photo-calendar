3ccfa54246a96c907e5be06acc856e79b073f6b7
Jens Svalgaard Kohrt <github@svalgaard.net>
2015-09-15 21:20:18 +0200

    Add keywords etc

--- setup.py
+++ setup.py
@@ -6,16 +6,21 @@ import codecs
 import re
 import os
 
+
+def rread(fn):
+    lfn = os.path.join(os.path.dirname(__file__) or '.', fn)
+    return codecs.open(lfn, 'r', 'utf-8').read()
+
 #
 # Get version number from __init__py
 #
-init = os.path.join(os.path.dirname(__file__) or '.', 'dpc', '__init__.py')
-init = codecs.open(init, 'r', 'utf-8').read()
+init = rread('dpc/__init__.py')
 VERSION = re.search("__version__ = '([^']+)'", init).group(1)
 
 setup(name='dpc',
       version=VERSION,
       description='Daily photo calendar',
+      long_description=rread('README.rst'),
       url='https://github.com/svalgaard/daily-photo-calendar',
       author='Jens Svalgaard Kohrt',
       author_email='python@svalgaard.net',
@@ -24,9 +29,11 @@ setup(name='dpc',
       zip_safe=False,
       requires=['Pillow'],
       scripts=['dpc-single'],
+      keywords='photos calendar',
       classifiers=[
           'Development Status :: 4 - Beta',
           'Intended Audience :: End Users/Desktop',
           'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.4',
           'License :: OSI Approved :: MIT License',
       ])
